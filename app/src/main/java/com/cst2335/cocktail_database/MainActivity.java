ca490f0b1759475e66395a189cc108cd27d4b5ed
Jimmy Cheng <chen0612@algonquinlive.com>
2022-08-02 13:37:44 -0400

    Initial commit

---
+++ app/src/main/java/com/cst2335/cocktail_database/MainActivity.java
@@ -0,0 +1,39 @@
+package com.cst2335.cocktail_database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+/*
+    Your application should have an EditText for entering the name of a drink. There should also be a “search”
+button which sends the search term to the server and returns a list of drinks that match that name.
+
+    The URL for searching is “https://www.thecocktaildb.com/api/json/v1/1/search.php?s=....”
+where … is the name that the user typed in.
+
+    Your application must show a list of results in a RecyclerView, with each row showing a different result returned.
+
+    If the user selects a drink from the list, your application will show the thumbnail picture,
+the instructions for making the drink, and the first 3 ingredients from the list of ingredients.
+
+    Also in the detail fragment, the user can save the data to the device for offline viewing.
+The user must be able to view a list of their saved data and remove them from the database of favourites if they choose.
+
+The SharedPreferences should save the user’s search words so that the next time you start the application,
+the previous search term is shown.
+*/
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}

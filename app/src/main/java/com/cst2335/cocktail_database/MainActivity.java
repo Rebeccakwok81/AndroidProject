0c78a69fc39068b62d4ed535abca6da5a991f440
Jimmy Cheng <chen0612@algonquinlive.com>
2022-08-02 16:39:12 -0400

    Project

--- app/src/main/java/com/cst2335/cocktail_database/MainActivity.java
+++ app/src/main/java/com/cst2335/cocktail_database/MainActivity.java
@@ -3,7 +3,18 @@ package com.cst2335.cocktail_database;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
     Your application should have an EditText for entering the name of a drink. There should also be a “search”
@@ -26,14 +37,61 @@ the previous search term is shown.
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> pic = new ArrayList<>();
+    ArrayAdapter <String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ListView list =  (ListView) findViewById(R.id.listDrink);
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                        android.R.layout.simple_list_item_1, pic);
 
+        Button clickBtnSearch = findViewById(R.id.btnSearch);
+         clickBtnSearch.setOnClickListener ( click-> {
 
+             EditText search = findViewById(R.id.etSearch);
 
+             pic.add(search.getText().toString());
+             list.setAdapter(arrayAdapter);
+             arrayAdapter.notifyDataSetChanged();
+        });
     }
+
+    /*
+    private class MyListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return pic.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return pic.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            LayoutInflater inflater = getLayoutInflater();
+            View newView;
+            TextView tView;
+
+            newView = inflater.inflate(R.layout.listDrink, viewGroup, false);
+            tView = newView.findViewById(R.id.etSearch);
+            tView.setText(getItem(i).toString());
+
+            return newView;
+        }
+    }
+
+     */
 }
